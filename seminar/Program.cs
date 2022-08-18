c85ce2bd2df3b58c5baec92f75b52865aec510f1
Artem <pointers@bk.ru>
2022-08-18 22:23:58 +0300

    seminar

---
+++ seminar/Program.cs
@@ -0,0 +1,16 @@
+﻿// Console.WriteLine ("Введите целое число:");
+// int N = Convert.ToInt32 (Console.ReadLine());
+// int num = (-1*N);
+// int count = num;
+
+// while (count<=N)
+// {
+//     Console.Write ($"bynth{count}ertg");
+//     count=count+1;
+// }
+
+Console.WriteLine ("Введите первое трехзначное число:");
+int a = Convert.ToInt32 (Console.ReadLine());
+
+
+
