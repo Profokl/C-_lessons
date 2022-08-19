7ab23b9f48320288771de40a2d6efebeb6462d7f
Artem <pointers@bk.ru>
2022-08-19 17:26:13 +0300

    Задача 3 решена

--- Domashka/Zadacha6/Program.cs
+++ Domashka/Zadacha6/Program.cs
@@ -3,3 +3,16 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+
+Console.WriteLine ("Введите число:");
+int a = Convert.ToInt32 (Console.ReadLine());
+
+if (a%2 == 0)
+ { 
+    Console.WriteLine ("да");
+ }
+ else
+ {
+    Console.WriteLine ("нет");
+ }
