122a4561edc166080da30f81d50f0240da17072a
Artem <pointers@bk.ru>
2022-08-24 11:19:33 +0300

    Задача 10 решена

---
+++ Domashka/Zadacha10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Console.WriteLine("Введите трехзначное число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// int result = a / 10;
+// int result1 = result % 10;
+// Console.WriteLine(result1);
+
+int Second (int arg)
+{
+   int result = arg / 10;
+   int result1 = result % 10; 
+   return result1;
+}
+
+Console.WriteLine("Введите трехзначное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Second (a);
+Console.WriteLine(Second(a));
