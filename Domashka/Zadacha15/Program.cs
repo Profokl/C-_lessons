9dc1ec5a0b9d7d3c88289a4e3d8f129646b446e0
Artem <pointers@bk.ru>
2022-08-25 14:05:45 +0300

    задача 15 решена

---
+++ Domashka/Zadacha15/Program.cs
@@ -0,0 +1,48 @@
+﻿// ПРОСТОЕ РЕШЕНИЕ
+
+// Console.WriteLine("Введите номер дня недели:");
+// int week = Convert.ToInt32(Console.ReadLine());
+
+// if (week < 6)
+// {
+//     Console.WriteLine("будний день");
+// }
+// else
+// {
+//     Console.WriteLine("выходной день");
+// }
+// if (week>7)
+// {
+//     Console.WriteLine ("нет такого дня недели");
+// }
+
+//АЛЬТЕРНАТИВНОЕ РЕШЕНИЕ
+
+Console.WriteLine("Введите номер дня недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+int Week(int week)
+{
+    if (week > 7)
+    {
+        Console.WriteLine("нет такого дня недели");
+        return(0);
+    }
+    if (week < 6)
+    {
+        Console.WriteLine("будний день");
+        return(0);
+    }
+    else
+    {
+        Console.WriteLine("выходной день");
+        return (0);
+    }
+    
+}
+
+Week(day);
+
+
+
+
