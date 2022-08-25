f0445a4be1cc23ee7f753133d62880895b21d518
Artem <pointers@bk.ru>
2022-08-25 14:11:30 +0300

    добавил комментарии

--- Domashka/Zadacha10/Program.cs
+++ Domashka/Zadacha10/Program.cs
@@ -1,10 +1,13 @@
-﻿// Console.WriteLine("Введите трехзначное число:");
+﻿//БЕЗ МЕТОДА
+
+// Console.WriteLine("Введите трехзначное число:");
 // int a = Convert.ToInt32(Console.ReadLine());
 
 // int result = a / 10;
 // int result1 = result % 10;
 // Console.WriteLine(result1);
 
+// С МЕТОДОМ
 int Second (int arg)
 {
    int result = arg / 10;
