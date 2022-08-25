19272d53cc842033930bc89e076369c3bfdf8ca4
Artem <pointers@bk.ru>
2022-08-25 12:40:21 +0300

    13 задача решена!!!

---
+++ Domashka/Zadacha11/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+string str = N.ToString();
+int[] array = new int[str.Length];
+int count = 0;
+while (count<str.Length)
+{
+    array[count] = Convert.ToInt32(str[count].ToString());
+    count++;
+}
+Console.WriteLine();
+if(str.Length>2)
+{
+    Console.WriteLine(array[2]);
+}
+else
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
